e121490c3ae2bccd4acd6ba10e19bc5be2f990c7
akshaychavan23031998 <akshayrchavan07@gmail.com>
2025-04-18 00:41:39 +0530

    S2EP18(DevTinder UI Part 4): Accept or Reject the connection Reuest API Integration.

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -2,11 +2,26 @@ import axios from "axios";
 import React, { useEffect } from "react";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { addRequests } from "../utils/requestSlice";
+import { addRequests, removeRequest } from "../utils/requestSlice";
 
 const Requests = () => {
   const dispatch = useDispatch();
   const requests = useSelector((store) => store.requests);
+
+  // http://localhost:7777/request/review/accepted/67fe7b800a189a093636880a
+  const reviewReqest = async (status, _id) => {
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/request/review/${status}/${_id}`,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeRequest(_id));
+      // window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   const fetchRequests = async () => {
     try {
@@ -28,11 +43,11 @@ const Requests = () => {
   if (!requests) return;
 
   if (requests.length === 0)
-    return <h1 className="text-bold text-2xl">No Connection Request Found</h1>;
+    return <h1 className="text-bold flex m-10 justify-center text-2xl">No Connection Request Found</h1>;
 
   return (
     <div className="text-center my-10">
-      <h1 className="text-bold text-3xl text-black">Connections</h1>
+      <h1 className="text-bold text-3xl text-black">Requests</h1>
 
       {requests.map((request) => {
         const { firstName, lastName, photo, about } = request.fromUserId;
@@ -50,12 +65,24 @@ const Requests = () => {
               />
             </div>
             <div className="text-left mx-4">
-              <h2 className="font-bold text-2xl">{firstName + " " + lastName}</h2>
+              <h2 className="font-bold text-2xl">
+                {firstName + " " + lastName}
+              </h2>
               <p className="text-sm text-gray-600">{about}</p>
             </div>
             <div>
-                <button className="btn btn-primary mx-2">Accept</button>
-                <button className="btn btn-secondary mx-2">Reject</button>
+              <button
+                className="btn btn-primary mx-2"
+                onClick={() => reviewReqest("accepted", request._id)}
+              >
+                Accept
+              </button>
+              <button
+                className="btn btn-secondary mx-2"
+                onClick={() => reviewReqest("rejected", request._id)}
+              >
+                Reject
+              </button>
             </div>
           </div>
         );
@@ -65,3 +92,5 @@ const Requests = () => {
 };
 
 export default Requests;
+
+//accepted rejected
